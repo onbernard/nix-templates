4c30eefc11ca8cff97ac60ae6aae3a52dc63c0d5
Onésime BERNARD <onesime.bernard@ima.it>
2025-04-07 08:55:59 +0000

    Update typst template

--- typst/flake.nix
+++ typst/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Typst flake";
+  description = "Typst template";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -14,11 +14,26 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; overlays = []; };
     in
     pkgs.mkShell {
-      buildInputs = with pkgs; [
+      packages = with pkgs; [
         typst        
         atkinson-hyperlegible
         atkinson-monolegible
         font-awesome
+        (pkgs.writeShellScriptBin "startTypstWatch" ''
+          if [ "$#" -eq 0 ]; then
+            echo "Usage: $0 file1.typ file2.typ ..."
+            exit 1
+          fi
+          > pids.txt
+          for file in "$@"; do
+            typst watch "$file" &
+            echo $! >> pids.txt
+          done
+        '')
+        (pkgs.writeShellScriptBin "stopTypstWatch" ''
+          cat pids.txt | xargs kill
+          rm pids.txt
+        '')
       ];
       shellHook = ''
         export TYPST_FONT_PATHS="/home/$USER/.nix-profile/share/fonts:${pkgs.font-awesome}"
