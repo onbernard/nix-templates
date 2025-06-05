980b03e4a9b48a189d3e445ceffd94e867f5bd7d
BernardO <onesime.bernard@ima.it>
2025-06-05 13:08:30 +0000

    Add stdenv.cc.cc and LD_LIBRARY_PATH to rust packages

--- rust/flake.nix
+++ rust/flake.nix
@@ -10,43 +10,46 @@
     };
   };
 
-  outputs = inputs@{ self, ... }:
-  with inputs;
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; overlays = [
-      rust-overlay.overlays.default
-      (final: prev: {
-        rustToolchain = final.rust-bin.stable.latest.default.override { extensions = [ "rust-src"]; };
-      })
-    ]; };
-  in
-  {
-    devShell = pkgs.mkShell {
-      packages = with pkgs; [
-        # (pkgs.rustPlatform.buildRustPackage {
-        #   pname = ""; # Copy from cargo.toml
-        #   version = ""; # Copy from cargo.toml
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "";
-        #     repo = "";
-        #     rev = ""; # branch
-        #     hash = "";
-        #   };
-        #   cargoHash = "";
-        # })
-        rustToolchain
-        cargo-bloat
-        cargo-edit
-        cargo-outdated
-        cargo-udeps
-        cargo-watch
-        rust-analyzer
-      ];
-      env = {
-        RUST_BACKTRACE = "1";
-        RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-      };
-    };
-  });
+  outputs = inputs @ {self, ...}:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+            (final: prev: {
+              rustToolchain = final.rust-bin.stable.latest.default.override {extensions = ["rust-src"];};
+            })
+          ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            # (pkgs.rustPlatform.buildRustPackage {
+            #   pname = ""; # Copy from cargo.toml
+            #   version = ""; # Copy from cargo.toml
+            #   src = pkgs.fetchFromGitHub {
+            #     owner = "";
+            #     repo = "";
+            #     rev = ""; # branch
+            #     hash = "";
+            #   };
+            #   cargoHash = "";
+            # })
+            rustToolchain
+            cargo-bloat
+            cargo-edit
+            cargo-outdated
+            cargo-udeps
+            cargo-watch
+            rust-analyzer
+            stdenv.cc.cc.lib
+          ];
+          env = {
+            RUST_BACKTRACE = "1";
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+          };
+        };
+      });
 }
