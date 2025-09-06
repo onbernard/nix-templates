1b072538b8d393dd4ed3c08c6e354329a17a2626
Onésime BERNARD <bernard.onesime@gmail.com>
2025-09-06 14:03:11 +0200

    Update uv helix config

--- python-uv/flake.nix
+++ python-uv/flake.nix
@@ -6,18 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, ... }:
-  with inputs;
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; overlay = [];};
-  in
-  {
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [uv];
-      shellHook = ''
-        uv sync && source .venv/bin/activate
-      '';
-    };
-  });
+  outputs = inputs @ {self, ...}:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlay = [];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            dprint # Markdown
+            alejandra # Nix code formatter
+            nixd # Nix language server
+            taplo # TOML toolkit
+            pyright # Python type checker
+            ruff # Python linter
+            black # Python code formatter
+          ];
+          shellHook = ''
+            uv sync && source .venv/bin/activate
+          '';
+        };
+      });
 }
