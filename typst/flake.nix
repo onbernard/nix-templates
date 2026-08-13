930111b2d2bd6349fc3b551f6a5ffe004eebf5cd
Onésime BERNARD <bernard.onesime@gmail.com>
2026-08-13 18:10:04 +0200

    Update typst template

--- typst/flake.nix
+++ typst/flake.nix
@@ -3,41 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";    
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, ... }:
-  with inputs;
-  {
-    devShells.x86_64-linux.default = 
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = []; };
-    in
-    pkgs.mkShell {
-      packages = with pkgs; [
-        typst        
-        atkinson-hyperlegible
-        atkinson-monolegible
-        font-awesome
-        (pkgs.writeShellScriptBin "startTypstWatch" ''
-          if [ "$#" -eq 0 ]; then
-            echo "Usage: $0 file1.typ file2.typ ..."
-            exit 1
-          fi
-          > pids.txt
-          for file in "$@"; do
-            typst watch "$file" &
-            echo $! >> pids.txt
-          done
-        '')
-        (pkgs.writeShellScriptBin "stopTypstWatch" ''
-          cat pids.txt | xargs kill
-          rm pids.txt
-        '')
-      ];
-      shellHook = ''
-        export TYPST_FONT_PATHS="/home/$USER/.nix-profile/share/fonts:${pkgs.font-awesome}"
-      '';
-    };
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          dprint # Markdown formatter
+          alejandra # Nix formatter
+          nixd # Nix language server
+          taplo # TOML toolkit
+          typst
+          atkinson-hyperlegible
+          atkinson-monolegible
+          font-awesome
+        ];
+        shellHook = ''
+          export TYPST_FONT_PATHS="${pkgs.atkinson-hyperlegible}/share/fonts:${pkgs.atkinson-monolegible}/share/fonts:${pkgs.font-awesome}/share/fonts"
+        '';
+      };
+    });
 }
